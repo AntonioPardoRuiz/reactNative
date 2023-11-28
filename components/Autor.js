c6c9566b01a46274898374eb54ce669366727299
Antonio Miguel Pardo Ruiz <a2161@inycom.es>
2023-11-28 09:47:03 +0100

    Añadimos el listado en autor

--- components/Autor.js
+++ components/Autor.js
@@ -2,17 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import { Icon } from '@rneui/themed';
 import { StyleSheet, Text, View, Button, SectionList } from 'react-native';
 import { Text, Card, Button, Icon } from '@rneui/themed';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { ListItem, Avatar } from '@rneui/themed';
 //Importamos los datos del proceso
 import { AUTORES } from '../shared/autores';
 
 //Informacion del proceso
 
-
-
 export default function Autor() {
   return (
     <View style={styles.container}>
+
       <Text>Autor</Text>
       <Card>
         <Card.Title>Informacion</Card.Title>
@@ -21,24 +21,27 @@ export default function Autor() {
           Aplicacion desarrollada por Antonio M.Pardo Ruiz. A continuacion
           puede consultar mas informacion sobre el autor.
         </Text>
-        <ScrollView>
-        {
-          AUTORES.map((autores) => (
-            <ListItem
-              bottomDivider
-              key={autores.id}
-              onPress={() => console.log('Element ' + product.id + ' selected')}
-            >
-              <Avatar source={{ uri: product.image }} />
-              <ListItem.Content>
-                <ListItem.Title>{product.name}</ListItem.Title>
-              </ListItem.Content>
-              <ListItem.Chevron />
-            </ListItem>
-          ))
-        }
-      </ScrollView>
-
+        <SafeAreaProvider>
+          <SafeAreaView style={styles.container}>
+            <ScrollView>
+              {
+                AUTORES.map((autores) => (
+                  <ListItem
+                    bottomDivider
+                    key={autores.id}
+                    onPress={() => console.log('Element ' + product.id + ' selected')}
+                  >
+                    <Avatar source={{ uri: product.image }} />
+                    <ListItem.Content>
+                      <ListItem.Title>{product.name}</ListItem.Title>
+                    </ListItem.Content>
+                    <ListItem.Chevron />
+                  </ListItem>
+                ))
+              }
+            </ScrollView>
+          </SafeAreaView>
+        </SafeAreaProvider>
       </Card>
     </View>
   );
