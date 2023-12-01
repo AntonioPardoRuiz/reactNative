b1d3c09e93d7e1ed5982cd84a54d426e1d0d187e
Antonio Miguel Pardo Ruiz <a2161@inycom.es>
2023-12-01 12:51:16 +0100

    añadimos el servicio

--- components/Videoclub.js
+++ components/Videoclub.js
@@ -1,31 +1,72 @@
+
+import React from "react";
 import { StatusBar } from 'expo-status-bar';
 import { Icon } from '@rneui/themed';
 import { StyleSheet, Text, View, Button } from 'react-native';
 //Importamos los datos del proceso
 import { MOVIES } from '../shared/peliculas';
+import { CONFIG } from '../shared/config';
 
-export default function Videoclub() {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.titleText}>Videoclub</Text>
-            <SafeAreaProvider>
-                <SafeAreaView style={styles.container}>
-                    <ScrollView>
-                        {
-                            MOVIES.map((peliculas) => (
-                                <Card>
-                                    <ListItem
-                                        bottomDivider
-                                        key={peliculas.id}
-                                        onPress={() => console.log('Element ' + peliculas.id + ' selected')}
+export default class Videoclub extends React.Component {
+
+    //Definimos el constructor.
+
+    constructor(props){
+        super(props);
+        this.state = {
+            peliculas:[],
+            isLoading:false,
+            error:false
+        };
+    }
+    //Utilizamos esta opcion para la carga de datos sin utilizar el constructor.
+    componentDidMount() {
+        this.loadData();
+    }
+
+    async loadData() {
+        try {
+          let response = await fetch(CONFIG.baseUrl + "/catalog");
+          let data = await response.json();
+          console.log(data);
+          this.setState({ peliculas: data, isLoading: false });
+        } catch (error) {
+          console.log(error);
+          this.setState({ error: error, isLoading: false });
+        }
+      }
+
+      render() {
+        //Generamos las variables de estado que cambian a lo largo del ciclo de la pagina.
+        const { peliculas, error, isLoading } = this.state;
+
+        if (error)
+            return <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }} ><Text>{error.message}</Text></View>;
+
+        if (isLoading)
+                return <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }} ><Text>Loading...</Text></View>;
+            
+                return (
+                    <View style={styles.container}>
+                        <Text style={styles.titleText}>Videoclub</Text>
+                        <SafeAreaProvider>
+                        <SafeAreaView style={styles.container}>
+                            <ScrollView>
+                                {
+                                    peliculas.map((pelicula) => (
+                                    <Card>
+                                        <ListItem
+                                          bottomDivider
+                                          key={pelicula.id}
+                                          onPress={() => console.log('Element ' + pelicula.id + ' selected')}
                                     >
-                                        <Avatar source={{ uri: peliculas.poster }} />
-                                        <ListItem.Content>
-                                            <Card.Title>{peliculas.title}</Card.Title>
-                                            <Card.Divider />
-                                            <Text style={styles.titleText}>Director: {peliculas.director}</Text>
-                                            <Text style={styles.titleText}>Año: {peliculas.year}</Text>
-                                        </ListItem.Content>
+                                         <Avatar source={{ uri: pelicula.poster }} />
+                                           <ListItem.Content>
+                                             <Card.Title>{pelicula.title}</Card.Title>
+                                             <Card.Divider />
+                                             <Text style={styles.titleText}>Director: {pelicula.director}</Text>
+                                             <Text style={styles.titleText}>Año: {pelicula.year}</Text>
+                                          </ListItem.Content>
                                         <ListItem.Chevron />
                                     </ListItem>
                                 </Card>
@@ -36,6 +77,7 @@ export default function Videoclub() {
             </SafeAreaProvider>
         </View >
       );
+  }
 }
 
 const styles = StyleSheet.create({
---
+++ shared/config.js
@@ -0,0 +1,6 @@
+export const CONFIG = {
+    //Url que usamos:
+    //http://gbrain.dlsi.ua.es/videoclub/api/v1/catalog
+    //baseUrl: 'https://pdm-reactnative.onrender.com'
+     baseUrl: 'http://gbrain.dlsi.ua.es/videoclub/api/v1/'
+   };
